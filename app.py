05d1ef6694b29a96773a3a2ab76127d22f75bebf
vrushalisutar26 <157814068+vrushalisutar26@users.noreply.github.com>
2026-03-10 17:23:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def make_call():
         return jsonify({'error': str(ex)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
